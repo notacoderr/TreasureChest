0840e734ac1052c391d0dc6e65bb5a6c68bf950a
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-02-01 23:00:56 +0800

    Update RefillTask.php

--- src/awzaw/treasurechest/RefillTask.php
+++ src/awzaw/treasurechest/RefillTask.php
@@ -12,43 +12,37 @@ class RefillTask extends Task {
 
 	public function __construct(Main $plugin){
 		    $this->plugin = $plugin;
-	 	}
+	}
 
-    public function onRun(int $tick) {
+   	public function onRun(int $tick) {
 
-        foreach ($this->plugin->getChests()->getAll() as $c => $chestmode) {
-            $c = explode(":", $c);
-            if (!(($lev = $this->plugin->getServer()->getLevelByName($c[3])) instanceof Level))
-                continue;
-            $tile = $lev->getTile(new Vector3((int) $c[0], (int) $c[1], (int) $c[2]));
-            if (!$tile)
-                continue;
-            if (!($tile instanceof Chest))
-                continue;
+        	foreach ($this->plugin->getChests()->getAll() as $c => $chestmode) {
+			$c = explode(":", $c);
+			if (!(($lev = $this->plugin->getServer()->getLevelByName($c[3])) instanceof Level)) continue;
+			$tile = $lev->getTile(new Vector3((int) $c[0], (int) $c[1], (int) $c[2]));
+			if (!$tile) continue;
+		    	if (!($tile instanceof Chest)) continue;
 
-            $tile->getInventory()->clearAll();
-            $inv = $tile->getInventory();
+		    	$tile->getInventory()->clearAll();
+		   	$inv = $tile->getInventory();
 
-            foreach ($this->plugin->treasure->getAll() as $treasure => $tarray) {
+		    	foreach ($this->plugin->treasure->getAll() as $treasure => $tarray) {
 
-                if ($treasure === $chestmode) {
-                    $i = 0;
-                    foreach ($tarray as $tstring) {
-                        $t = explode(":", $tstring);
-                        
-                        $amount = $t[1];
-                        if ($this->plugin->prefs->get("RandomizeAmount"))
-                            $amount = mt_rand(1, $amount);
-                        
-                        if (mt_rand(0, 100) < $t[2]) {
-                            $inv->setItem($i, ItemFactory::get($t[0], 0, $amount));
-                            $i++;
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-    }
+				if ($treasure === $chestmode) {
+					$i = 0;
+				    	foreach ($tarray as $tstring) {
+						$t = explode(":", $tstring);
 
+						$amount = $t[1];
+						if ($this->plugin->prefs->get("RandomizeAmount")) $amount = mt_rand(1, $amount);
+						if (mt_rand(0, 100) < $t[2]) {
+							$inv->setItem($i, ItemFactory::get($t[0], 0, $amount));
+							$i++;
+						}
+			    		}
+			    		break;
+				}
+            		}
+        	}
+    	}
 }
